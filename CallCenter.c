97c2c436df859e8100bb11673aab70ae6819c454
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-30 16:14:24 +0530

    Initialized main function with dynamic memory allocation for queue

--- CallCenter.c
+++ CallCenter.c
@@ -3,3 +3,27 @@
 void enqueue(int *queue, int *rear, int *count, int size, int ticket);
 void dequeue(int *queue, int *front, int *rear, int *count, int size);
 void display(int *queue, int front, int count, int size);
+int main() {
+int size;
+printf("Enter size of queue ");
+scanf("%d", &size);
+int *queue = (int *)malloc(size * sizeof(int));
+if (queue == NULL) {
+printf("Memory allocation failed!\n");
+return 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
