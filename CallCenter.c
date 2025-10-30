89dbeb3a77a125cf5c27bbe6e3c92d4eff5882fa
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-30 16:13:43 +0530

    Declare function prototypes for queue operations (enqueue, dequeue, display)

--- CallCenter.c
+++ CallCenter.c
@@ -1,1 +1,5 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+void enqueue(int *queue, int *rear, int *count, int size, int ticket);
+void dequeue(int *queue, int *front, int *rear, int *count, int size);
+void display(int *queue, int front, int count, int size);
